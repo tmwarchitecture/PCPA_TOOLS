5e2dc2b9e627d9c573e7de215b7721747e93d09d
tmwarchitecture <tmwarchitecture@gmail.com>
2018-04-18 23:04:33 -0400

    AddTitleBlock implemented

--- layout.py
+++ layout.py
@@ -1,6 +1,47 @@
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
 import Rhino
+import os.path
+import datetime
+
+def GetDatePrefix():
+    year = int(datetime.datetime.today().strftime('%Y'))-2000
+    md = datetime.datetime.today().strftime('%m%d')
+    return str(year) + str(md)
+
+def AddTitleBlock(size):
+    rs.EnableRedraw(False)
+    if size == 11:
+        ttlPathRaw = r'C:\Users\Tim\Google Drive\180413\PROJ_TBLK 11x17L.dwg'
+        if os.path.isfile(ttlPathRaw) == False:
+            print "Titleblock file not found"
+            return
+        ttlPath = '"' + ttlPathRaw + '"'
+        pt2 = [16.646, 10.646]
+    elif size == 8:
+        print "Titleblock not setup"
+        return
+    elif size == 18:
+        print "Titleblock not setup"
+        return
+    
+    layout = sc.doc.Views.GetPageViews()[-1]
+    if layout is None:  return
+    
+    #Insert block
+    rs.Command("-_Insert f "+ttlPath+" Block Enter 0,0,0 1.0 0.0", False)
+    rs.Command("-_SelLast Enter")
+    rs.Command("_Cut ")
+    sc.doc.Views.ActiveView = layout
+    rs.Command("_Paste ")
+    rs.UnselectAllObjects()
+    
+    #Add detail
+    pt1 = [.354, 1.417]
+    
+    rs.AddDetail(layout.ActiveViewportID, pt1, pt2, "PCPA " + str(layout.PageName), 7)
+    
+    rs.EnableRedraw(True)
 
 def AddLayout(size):
     if size == 11:
@@ -15,7 +56,10 @@ def AddLayout(size):
         name = '18x24 '
         width = '24 '
         height = '18 '
-    rs.Command('-_Layout ' + name + width + height + '0 ', False)
+    result = rs.Command('-_Layout ' + name + width + height + '0 ', False)
+    
+    if result:
+        AddTitleBlock(size)
 
 def BatchPrintLayouts():
     print "BatchPrintLayout is WIP. Use with caution."
@@ -25,7 +69,9 @@ def BatchPrintLayouts():
             print "No layouts in file"
             return
         
-        filename = rs.SaveFileName("Save", "PDF (*.pdf)|*.pdf||")
+        defaultName = GetDatePrefix() + "_Rhino"
+        
+        filename = rs.SaveFileName("Save", "PDF (*.pdf)|*.pdf||", filename = defaultName)
         if filename is None: return
         
         names = []
@@ -66,4 +112,3 @@ def main(func):
 if __name__ == "__main__":
     func = rs.GetInteger()
     main(func)
-    
