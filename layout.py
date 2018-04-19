c23a5f501101d3853b4bca3ca619c5b0dbf517fd
tmwarchitecture <tmwarchitecture@gmail.com>
2018-04-19 17:56:49 -0400

    Remove blocks
    No need for blocks. Draw lines instead.

--- layout.py
+++ layout.py
@@ -3,6 +3,9 @@ import scriptcontext as sc
 import Rhino
 import os.path
 import datetime
+import sys
+sys.path.append(r'X:\05_RHINO STANDARDS\05 SCRIPTS\PYTHON\PCPA')
+import PCPA
 
 def GetDatePrefix():
     year = int(datetime.datetime.today().strftime('%Y'))-2000
@@ -11,34 +14,125 @@ def GetDatePrefix():
 
 def AddTitleBlock(size):
     rs.EnableRedraw(False)
+    fileLocations = PCPA.config.GetDict()
     if size == 11:
-        ttlPathRaw = r'C:\Users\Tim\Google Drive\180413\PROJ_TBLK 11x17L.dwg'
+        ttlPathRaw = fileLocations['Titleblock 11x17L']
         if os.path.isfile(ttlPathRaw) == False:
             print "Titleblock file not found"
             return
         ttlPath = '"' + ttlPathRaw + '"'
-        pt2 = [16.646, 10.646]
+        offset = .354
+        row1 = offset
+        row2 = row1*2
+        row3 = row1*3
+        row4 = row1*4
+        leftEdge = offset
+        middle = 17/2
+        rightEdge = 17-offset
+        
+        pt1 = [leftEdge, row4]
+        pt2 = [rightEdge, 11-row1]
+        txtBase1 = [middle,row2]
+        txtBase2 = [leftEdge,row2]
+        txtBase3 = [rightEdge,row2]
+        txtBase4 = [leftEdge,row1]
+        txtBase5 = [rightEdge,row1]
+        
+        lineSt = [leftEdge, row3]
+        lineEnd = [rightEdge, row3]
+        
+        txtSizeL = .125
+        txtSizeM = .094
+    
     elif size == 8:
-        print "Titleblock not setup"
-        return
+        ttlPathRaw = fileLocations['Titleblock 8.5x11L']
+        if os.path.isfile(ttlPathRaw) == False:
+            print "Titleblock file not found"
+            return
+        ttlPath = '"' + ttlPathRaw + '"'
+        
+        offset = .229
+        row1 = offset
+        row2 = row1*2
+        row3 = row1*3
+        row4 = row1*4
+        leftEdge = offset
+        middle = 11/2
+        rightEdge = 11-offset
+        
+        pt1 = [leftEdge, row4]
+        pt2 = [rightEdge, 8.5-row1]
+        txtBase1 = [middle,row2]
+        txtBase2 = [leftEdge,row2]
+        txtBase3 = [rightEdge,row2]
+        txtBase4 = [leftEdge,row1]
+        txtBase5 = [rightEdge,row1]
+        
+        lineSt = [leftEdge, row3]
+        lineEnd = [rightEdge, row3]
+        
+        txtSizeL = .125
+        txtSizeM = .063
+        
     elif size == 18:
-        print "Titleblock not setup"
-        return
+        ttlPathRaw = fileLocations['Titleblock 18x24L']
+        if os.path.isfile(ttlPathRaw) == False:
+            print "Titleblock file not found"
+            return
+        ttlPath = '"' + ttlPathRaw + '"'
+        offset = .5
+        row1 = offset
+        row2 = row1*2
+        row3 = row1*3
+        row4 = row1*4
+        leftEdge = offset
+        middle = 24/2
+        rightEdge = 24-offset
+        
+        pt1 = [leftEdge, row4]
+        pt2 = [rightEdge, 18-row1]
+        txtBase1 = [middle,row2]
+        txtBase2 = [leftEdge,row2]
+        txtBase3 = [rightEdge,row2]
+        txtBase4 = [leftEdge,row1]
+        txtBase5 = [rightEdge,row1]
+        
+        lineSt = [leftEdge, row3]
+        lineEnd = [rightEdge, row3]
+        
+        txtSizeL = .250
+        txtSizeM = .125
     
     layout = sc.doc.Views.GetPageViews()[-1]
     if layout is None:  return
     
     #Insert block
-    rs.Command("-_Insert f "+ttlPath+" Block Enter 0,0,0 1.0 0.0", False)
-    rs.Command("-_SelLast Enter")
-    rs.Command("_Cut ")
+    #rs.Command("-_Insert f "+ttlPath+" Block Enter 0,0,0 1.0 0.0", False)
+    #rs.Command("-_SelLast Enter")
+    #rs.Command("_Cut ")
     sc.doc.Views.ActiveView = layout
-    rs.Command("_Paste ")
-    rs.UnselectAllObjects()
+    #rs.Command("_Paste ")
+    #rs.UnselectAllObjects()
+    
+    projectTitle = rs.GetDocumentData(section = "PCPA", entry = "Project Title")
+    if projectTitle is None:
+        projectTitle = "Project Title"
+    
+    clientName = rs.GetDocumentData(section = "PCPA", entry = "Client Name")
+    if clientName is None:
+        clientName = "Client Name"
+    
+    #Add text
+    rs.AddText("Title", txtBase1, txtSizeL, justification = 2)
+    rs.AddText(projectTitle, txtBase2, txtSizeL, justification = 1)
+    rs.AddText('%<Date("MMMM d, yyyy")>%', txtBase3, txtSizeM, justification = 4)
+    
+    rs.AddText(clientName, txtBase4, txtSizeM, justification = 1)
+    rs.AddText('COPYRIGHT %<Date("yyyy")>% Pelli Clarke Pelli Architects', txtBase5, txtSizeM, justification = 4)
+    
+    rs.AddLine(lineSt, lineEnd)
     
     #Add detail
-    pt1 = [.354, 1.417]
-    
     rs.AddDetail(layout.ActiveViewportID, pt1, pt2, "PCPA " + str(layout.PageName), 7)
     
     rs.EnableRedraw(True)
@@ -88,8 +182,11 @@ def BatchPrintLayouts():
         if stop == False:
             print "No layouts selected"
             return
-        
-        pdf = Rhino.FileIO.FilePdf.Create()
+        try:
+            pdf = Rhino.FileIO.FilePdf.Create()
+        except:
+            print "Failed to load Rhino.FileIO.FilePdf.Create()"
+            return
         dpi = 300
         for i, page in enumerate(pages):
                 if selectedLayouts[i][1]:
