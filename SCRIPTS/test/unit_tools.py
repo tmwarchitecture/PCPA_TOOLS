bc9c9a56b151bcc64da49488af07f2f52b288dc0
tmwarchitecture <tmwarchitecture@gmail.com>
2018-05-22 22:32:43 -0400

    Unit conversion functions

--- SCRIPTS/test/unit_tools.py
+++ SCRIPTS/test/unit_tools.py
@@ -1,15 +1,40 @@
 import rhinoscriptsyntax as rs
 
-def NumberToInch(numberString):
-    segments = numberString.split("'")
-    number = 0
-    for each in segments:
+def ConvertImperialLength(numberString, ToInches = True):
+    """
+    ConvertLength(numberString, ToInches = True)
+    -Acceptable formats:
+        4.5'
+        36.1"
+        4'6"
+    """
+    if ToInches:
+        footScale = 12
+        inchScale = 1
+    else:
+        footScale = 1
+        inchScale = 1/12
+    if '"' in numberString and "'" in numberString:
+        if numberString.find("'") < numberString.find('"'):
+            try:
+                values = numberString.split("'")
+                feetInches = float(values[0])*footScale
+                inches = float(values[-1].split('"')[0])*inchScale
+                return feetInches + inches
+            except:
+                pass
+    elif "'" in numberString:
         try:
-            number = int(each)*12
+            return float(numberString.split("'")[0])*footScale
         except:
             pass
-    finalFloat = number
-    return finalFloat
+    elif '"' in numberString:
+        try:
+            return float(numberString.split('"')[0])*inchScale
+        except:
+            pass
+    return None
+
 
 dim = rs.GetString("Input a dimension")
-print NumberToInch(dim)
+print ConvertImperialLength(dim, True)
