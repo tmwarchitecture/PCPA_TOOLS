1d2290573c3cec503dfa51107fe1ed6cbc2428f7
tmwarchitecture <tmwarchitecture@gmail.com>
2018-06-03 00:12:46 -0400

    Force Material to Layer
    First working material script

--- SCRIPTS/test/test_materials.py
+++ SCRIPTS/test/test_materials.py
@@ -1,81 +1,52 @@
 import rhinoscriptsyntax as rs
-import scriptcontext as sc
-import Rhino
+import os
 
-def loadMatsFromFile(path):
-    folder = r'X:\05_RHINO STANDARDS\00 GENERAL SETTINGS\PCPA Rhino Materials'
-    
-    
-    
-    rdk = rs.GetPlugInObject("Renderer Development Kit")
-    rdk.ContentLoadFromFile(path)
+def loadMatFromPath(path):
+    if os.path.isfile(path):
+        rdk = rs.GetPlugInObject("Renderer Development Kit")
+        rc = rdk.ContentLoadFromFile(path)
+        return rc
+    else: return None
 
-def AddMatToMatTable(name):
-    print "ADDING {}".format(name)
+def ApplyMaterialToLayer(material, layer):
+    matName = '"' + str(material) + '"'
+    layName = '"' + layer + '"'
+    rs.Command("-_RenderAssignMaterialToLayer " + str(matName) + " " + str(layName) + " ", False)
 
 def getMaterialNames():
     rdk = rs.GetPlugInObject("Renderer Development Kit")
-    #get list of all RDK data ids in the document
     rdkALL = rdk.FactoryList()
-    #rs.
-    #filtered materials ID list
     arrMatIDList = rdk.ContentList("material") 
-    #abc = Rhino.Render.RenderMaterialTable.Count
-    print len(arrMatIDList)
-    
-    materialDict = {}
-    
-    #get materials names
     arrMatNames = []
     for i in range(len(arrMatIDList)):
-        matTable = sc.doc.ActiveDoc.Materials
-        
-        #sc.doc.ActiveDoc.Materials.Add(
-        
-        arrMatNames.append( rdk.ContentInstanceName(arrMatIDList[i]) )
-        result = rdk.SetMaterialInstanceId( arrMatIDList[i],i)
-        name = rdk.ContentInstanceName(arrMatIDList[i])
-        print "A"
-    
-    matCount = sc.doc.ActiveDoc.Materials.Count
-    for i in range(matCount):
-        
-        x = rdk.MaterialInstanceId(i)
-        
-        print "A"
-        name = rs.MaterialName(i)
-        if name is None: continue
-        
-        found = False
-        for matName in arrMatNames:
-            if  matName == name:
-                materialDict[name] = i
-                found = True
-                continue
-        print "A"
-        if not found:
-            AddMatToMatTable(name)
-    print materialDict
-    
+        arrMatNames.append(rdk.ContentInstanceName(arrMatIDList[i]))
+    return arrMatNames
 
-def test():
-    
-    print "A"
-    #layer = rs.CurrentLayer()
-    #rs.LayerMaterialIndex(layer, 0)
-    
-    
-    #if index==-1: index = rs.AddMaterialToLayer(layer)
-    #print index
+def IsMaterial(materialName):
+    matNames = getMaterialNames()
+    for name in matNames:
+        if name == materialName:
+            return True
+    return False
 
-path = r'C:\Users\Tim\Desktop\temp\mats\airplane.rmtl'
-#loadMatsFromFile(path)
-#getMaterialNames()
-#test()
-#rs.AddMaterialToLayer(
+def ForceMaterialToLayer(materialName, mylayer):
+    """
+    Applies material by name to layer. If no layer found, it is imported.
+        materialName: str
+        mylayer: str
+    """
+    materialNameFull = materialName + '.rmtl'
+    dir = r'C:\Users\Tim\Desktop\temp\mats'
+    matpath = os.path.join(dir, materialNameFull)
+    
+    if IsMaterial(materialName):
+        ApplyMaterialToLayer(materialName, mylayer)
+    else:
+        result = loadMatFromPath(matpath)
+        if result is None:
+            print "Material {} not found".format(materialName)
+        else:
+            ApplyMaterialToLayer(materialName, mylayer)
 
-#1 LOAD ALL MATERIALS TO RDK
-#2 LOAD MATERIALS INTO MATERIAL TABLE
-    #IF THEY DONT ALREADY EXIST IN THERE
-#3 GET MATERIAL INDEX
-#4 APPLY MATERIAL TO LAYER BY INDEX
+if __name__ == "__main__":
+    ForceMaterialToLayer('Chicken', rs.AddLayer())
