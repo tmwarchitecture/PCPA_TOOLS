c01c2d5ed8a829b20afd3a6108bdf322117b321a
tmwarchitecture <tmwarchitecture@gmail.com>
2018-09-14 17:54:49 -0400

    Update

--- SCRIPTS/layout.py
+++ SCRIPTS/layout.py
@@ -7,7 +7,7 @@ import config
 from utils import GetDatePrefix
 
 __author__ = 'Tim Williams'
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 def AddTitleBlock(size):
     filelocations = config.GetDict()
